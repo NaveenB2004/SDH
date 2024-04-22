54cc0970073bf1ec9c1fcc814d636604a252c742
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2024-04-23 01:53:45 +0530

    updates

--- src/test/java/Server/Server.java
+++ src/test/java/Server/Server.java
@@ -8,23 +8,25 @@ import java.net.Socket;
 
 public class Server {
     public void connect(int port) {
-        try {
-            @Cleanup
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("Server : Server started on port " + port);
+        new Thread(() -> {
+            try {
+                @Cleanup
+                ServerSocket serverSocket = new ServerSocket(port);
+                System.out.println("Server : Server started on port " + port);
 
-            while (true) {
-                System.out.println("Server : Waiting for clients...");
-                Socket socket = serverSocket.accept();
-                System.out.println("Server : Client connected to server on  : " + socket.getRemoteSocketAddress());
+                while (true) {
+                    System.out.println("Server : Waiting for clients...");
+                    Socket socket = serverSocket.accept();
+                    System.out.println("Server : Client connected to server on  : " + socket.getRemoteSocketAddress());
 
-                ServerHelper serverHelper = new ServerHelper(socket);
+                    ServerHelper serverHelper = new ServerHelper(socket);
 
-                Thread serverThread = new Thread(serverHelper);
-                serverThread.start();
+                    Thread serverThread = new Thread(serverHelper);
+                    serverThread.start();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        }).start();
     }
 }
