72df22772fb575606f136f03028e34d5116f2608
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2024-04-23 00:33:15 +0530

    updates

--- src/main/java/com/naveenb2004/DataHandler.java
+++ src/main/java/com/naveenb2004/DataHandler.java
@@ -2,6 +2,7 @@ package com.naveenb2004;
 
 import lombok.*;
 
+import java.io.File;
 import java.io.Serializable;
 
 @Getter
@@ -11,9 +12,11 @@ public class DataHandler {
     @NonNull
     private final String request;
     @Setter(AccessLevel.PROTECTED)
-    private String timestamp;
-    private DataType dataType = DataType.NONE;
+    private long timestamp;
+    @NonNull
+    private DataType dataType;
     private Serializable data;
+    private File dataLocation;
 
     public enum DataType {
         NONE("0"),
@@ -27,11 +30,24 @@ public class DataHandler {
         }
     }
 
-    public void setDataType(@NonNull DataType dataType) {
-        this.dataType = dataType;
+    public void setData(Serializable data) {
+        if (this.dataType != DataType.OBJECT) {
+            throw new IllegalArgumentException("Invalid data type");
+        }
+        this.data = data;
     }
 
-    protected static String timestamp() {
-        return null;
+    public void setDataLocation(@NonNull File dataLocation) {
+        if (this.dataType != DataType.FILE) {
+            throw new IllegalArgumentException("Invalid data type");
+        }
+        if (!dataLocation.isFile()) {
+            throw new IllegalArgumentException("Invalid data location");
+        }
+        this.dataLocation = dataLocation;
+    }
+
+    protected static long timestamp() {
+        return 0L;
     }
 }
--- src/main/java/com/naveenb2004/SocketDataHandler.java
+++ src/main/java/com/naveenb2004/SocketDataHandler.java
@@ -5,6 +5,8 @@ import lombok.*;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RequiredArgsConstructor
 public abstract class SocketDataHandler implements Runnable, AutoCloseable {
@@ -16,7 +18,7 @@ public abstract class SocketDataHandler implements Runnable, AutoCloseable {
     private static int defaultBufferSize = 1024;
     @Getter
     @Setter
-    private static int maxDataChunk = 5 * 1024 * 1024;
+    private static File tempFolder = new File("Temp");
 
     public abstract void receive(@NonNull DataHandler update);
 
@@ -61,7 +63,6 @@ public abstract class SocketDataHandler implements Runnable, AutoCloseable {
 
         StringBuilder header = new StringBuilder("{");
         header.append(dataHandler.getRequest().getBytes(StandardCharsets.UTF_8).length).append(",");
-        header.append(dataHandler.getTimestamp().getBytes(StandardCharsets.UTF_8).length).append(",");
         header.append(dataHandler.getDataType().value.getBytes(StandardCharsets.UTF_8).length).append(",");
 
         if (dataHandler.getDataType() == DataHandler.DataType.NONE) {
@@ -74,7 +75,9 @@ public abstract class SocketDataHandler implements Runnable, AutoCloseable {
             oOut.flush();
             header.append(out.toByteArray().length);
         } else if (dataHandler.getDataType() == DataHandler.DataType.FILE) {
-            header.append(((File) dataHandler.getData()).length());
+            File file = (File) dataHandler.getData();
+            String suffix = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+            header.append(suffix).append("$").append(file.length());
         }
         header.append("}");
         header.append(dataHandler.getRequest()).append(dataHandler.getTimestamp())
@@ -86,24 +89,84 @@ public abstract class SocketDataHandler implements Runnable, AutoCloseable {
         return out.toByteArray();
     }
 
+    @SneakyThrows
     @Override
     public void run() {
-        try {
-            InputStream in = SOCKET.getInputStream();
-            while (true) {
-                if (in.read() == '{') {
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    byte[] b = new byte[1];
-                    while (in.read(b) != '}') {
-                        out.write(b);
+        InputStream in = SOCKET.getInputStream();
+        ByteArrayOutputStream out;
+        byte[] buff;
+        while (true) {
+            if (in.read() == '{') {
+                out = new ByteArrayOutputStream();
+                buff = new byte[1];
+                while (in.read(buff) != '}') {
+                    out.write(buff);
+                }
+
+                String[] headers = out.toString(StandardCharsets.UTF_8).split(",");
+                int requestLen = Integer.parseInt(headers[0]);
+                int timestampLen = 5;
+                String dataTypeVal = headers[2];
+                int dataLen = Integer.parseInt(headers[3]);
+
+                buff = new byte[requestLen];
+                in.read(buff);
+                String request = new String(buff, StandardCharsets.UTF_8);
+
+                buff = new byte[timestampLen];
+                in.read(buff);
+                long timestamp = Long.parseLong(new String(buff, StandardCharsets.UTF_8));
+
+                DataHandler.DataType dataType = DataHandler.DataType.valueOf(dataTypeVal);
+
+                DataHandler dh = new DataHandler(request, dataType);
+                dh.setTimestamp(timestamp);
+
+                out = new ByteArrayOutputStream();
+                if (dataType == DataHandler.DataType.OBJECT) {
+                    buff = new byte[defaultBufferSize];
+                    while (true) {
+                        if (dataLen <= defaultBufferSize) {
+                            buff = new byte[dataLen];
+                            in.read(buff);
+                            out.write(buff);
+                            break;
+                        } else {
+                            in.read(buff);
+                            out.write(buff);
+                        }
+                        dataLen -= defaultBufferSize;
+                    }
+                    dh.setData(out.toByteArray());
+                } else if (dataType == DataHandler.DataType.FILE) {
+                    buff = new byte[1];
+                    while (in.read(buff) != '$') {
+                        out.write(buff);
                     }
 
-                    String[] headers = out.toString(StandardCharsets.UTF_8).split(",");
-
+                    Path tempFile = Files.createTempFile(tempFolder.toPath(), null,
+                            out.toString(StandardCharsets.UTF_8));
+                    @Cleanup
+                    FileOutputStream fos = new FileOutputStream(tempFolder);
+                    @Cleanup
+                    BufferedOutputStream bos = new BufferedOutputStream(fos);
+                    while (true) {
+                        if (dataLen <= defaultBufferSize) {
+                            buff = new byte[dataLen];
+                            in.read(buff);
+                            bos.write(buff);
+                            break;
+                        } else {
+                            in.read(buff);
+                            bos.write(buff);
+                        }
+                        dataLen -= defaultBufferSize;
+                    }
+                    dh.setDataLocation(tempFile.toFile());
                 }
+
+                receive(dh);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
