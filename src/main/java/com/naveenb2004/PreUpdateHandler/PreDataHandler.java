13e881e6bf111d1d7a8d4f5e4a3aad9fc38c3142
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2024-04-27 03:04:03 +0530

    updates

--- src/main/java/com/naveenb2004/PreUpdateHandler/PreDataHandler.java
+++ src/main/java/com/naveenb2004/PreUpdateHandler/PreDataHandler.java
@@ -1,30 +1,41 @@
 package com.naveenb2004.PreUpdateHandler;
 
 import com.naveenb2004.DataHandler;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 
 @Getter
 public class PreDataHandler {
-
+    private final PreUpdateHandler preUpdateHandler;
     private final long UUID;
     private final String request;
-    @Setter(AccessLevel.PROTECTED)
-    private DataHandler.DataType dataType;
-    @Setter(AccessLevel.PROTECTED)
+    private final DataHandler.DataType dataType;
     private long totalDataSize = -1L;
-    @Setter(AccessLevel.PROTECTED)
     private long transferredDataSize;
-    @Setter(AccessLevel.PROTECTED)
     private boolean isCompleted;
 
-    public PreDataHandler(long UUID, @NonNull String request, @NonNull DataHandler.DataType dataType) {
+    public PreDataHandler(@NonNull PreUpdateHandler preUpdateHandler, long UUID, @NonNull String request,
+                          @NonNull DataHandler.DataType dataType) {
+        this.preUpdateHandler = preUpdateHandler;
         this.UUID = UUID;
         this.request = request;
         this.dataType = dataType;
 
-        new PreUpdateHandler().update(this);
+        preUpdateHandler.update(this);
+    }
+
+    protected void setTotalDataSize(long totalDataSize) {
+        this.totalDataSize = totalDataSize;
+        preUpdateHandler.update(this);
+    }
+
+    protected void setTransferredDataSize(long transferredDataSize) {
+        this.transferredDataSize = transferredDataSize;
+        preUpdateHandler.update(this);
+    }
+
+    protected void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+        preUpdateHandler.update(this);
     }
 }
